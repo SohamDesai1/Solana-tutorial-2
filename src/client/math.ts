48931f06862203f68e48e5be986e0600ccf7b3c4
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-14 20:27:05 +0530

    created off chain code

---
+++ src/client/math.ts
@@ -0,0 +1,18 @@
+import {
+    Keypair,
+    Connection,
+    PublicKey,
+    LAMPORTS_PER_SOL,
+    SystemProgram,
+    TransactionInstruction,
+    Transaction,
+    sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import {
+    createKeypairFromFile,
+} from '../client/util';
+import fs from 'mz/fs';
+import os from 'os';
+import path from 'path';
+import yaml from 'yaml';
+
